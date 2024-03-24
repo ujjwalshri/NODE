8812148c0035a97a2fc4cebeb5e0e025bb23fca6
ujjwalshri <128953832+ujjwalshri@users.noreply.github.com>
2024-03-25 00:38:21 +0530

    api completed with mongo

--- REST_API/index.js
+++ REST_API/index.js
@@ -4,8 +4,14 @@ const app = express();
 const mongoose = require('mongoose')
 const PORT = 8002;
 const fs = require('fs');
+
+//Connection 
+mongoose.connect('mongodb://127.0.0.1:27017/myDataBase').then(()=>console.log("Mongo db connected")).catch((err)=> console.log(err))
+
+
+
+
 //Schema 
- 
 const userSchema = new mongoose.Schema({
     first_name : {
         type: String,
@@ -25,27 +31,32 @@ const userSchema = new mongoose.Schema({
     gender : {
         type: String,
     },
-})
+    
+},{timestamps:true});
 
+//Model 
+const User = new mongoose.model('user', userSchema);
 
 
 
 // middle ware - plugin
 app.use(express.urlencoded({extended:false}));
 // routes 
-app.get('/api/users', (req,res)=>{
+app.get('/api/users', async(req,res)=>{
+    const allUsers = await User.find({});
     res.setHeader("X-myName","ujjwal Shrivastava");// This is a custom header 
-    return res.json(users);
+    return res.json(allUsers);
 })
 app.use((req, res, next)=>{
     fs.appendFile("log.txt",`\n ${Date.now()} ${req.method} ${req.ip} ${req.path}`, (err, data)=>{
         next();
     })
 })
-app.get('/users' , (req,res)=>{
+app.get('/users' ,async (req,res)=>{
+    const allUsers = await User.find({});
     let html = `
     <ul>
-    ${users.map(user => `<li>${user.first_name} <li>`).join("")}
+    ${allUsers.map(user => `<li>${user.first_name} - ${user.email}<li>`).join("")}
     <ul>
     `
     return res.send(html);
@@ -53,51 +64,44 @@ app.get('/users' , (req,res)=>{
 
 
 
-app.route('/api/users/:id').get((req,res)=>{
-    let id = Number(req.params.id);
-    let user = users.find((user) => user.id === id );
+app.route('/api/users/:id').get( async(req,res)=>{
+
+    let user =  await User.findById(req.params.id);
     if(!user){
         return res.status(404).json({err:"user not found"});
     }
     return res.json(user);
-}).patch((req,res)=>{
+}).patch(async(req,res)=>{
     //todo : we have to add a new user
-    return res.status(503).json({status : "Not implemented"});
-}).delete((req, res) => {
+    const user= await User.findByIdAndUpdate(req.params.id, {last_name:"ullu"});
+    return res.status(202).json({mssg : "sucess"});
+}).delete(async(req, res) => {
     // Get the ID of the user to be deleted from request params
-    const id = req.params.id;
-
+    
+    
     // Find the index of the user with the given ID in the array
-    const index = users.findIndex(user => user.id === Number(id));
+    const user = await User.findByIdAndDelete(req.params.id);
 
     // If the user exists in the array, remove it
-    if (index !== -1) {
-        users.splice(index, 1);
-
-        // Write the updated array back to the JSON file
-        fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err) => {
-            if (err) {
-                return res.status(500).json({ status: "error", message: "Failed to delete user." });
-            }
-            return res.json({ status: "success", message: "User deleted successfully." });
-        });
-    } else {
-        return res.status(404).json({ status: "error", message: "User not found." });
-    }
+    
+   return res.status(200).json({mssg : "deleted"});
 });
 
 
-app.post('/api/users' , (req,res)=>{
+app.post('/api/users' , async(req,res)=>{
     //todo : we have to add a new user
     const body = req.body;
     if(!body || !body.first_name || !body.email){
         return res.status(400).json({msg:"First name , last name are required"});
     }
-    users.push({...body, id:users.length+1});
-    fs.writeFile('./MOCK_DATA.json' , JSON.stringify(users),(err, data)=>{
-        return res.json({status:"pending"});
+    const result = await User.create({
+        first_name :body.first_name,
+        last_name : body.last_name,
+        email : body.email,
+        gender: body.gender,
+        jobTitle : body.jobTitle,
     })
-    return res.status(201).json({status : "success" , id: users.length});
+    return res.status(201).json({mssg : "user created"});
 });
 
 app.listen(PORT,()=>console.log(`Server is started on the port ${PORT}`))
